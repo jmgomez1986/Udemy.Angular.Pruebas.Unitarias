9f435c441c89b7fef5a8ec2c3e2a5ab2233722da
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2021-07-17 23:49:24 -0300

    Configurar TestBed

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,25 @@
+import { CartComponent } from './cart.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BookService } from '../../services/book.service';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('Cart component', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [CartComponent],
+      providers: [BookService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
